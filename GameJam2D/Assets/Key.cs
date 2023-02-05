95bb266227ae9628fd3afab679cec95fb1d622bc
mbowyer17 <matthewbowyer17@gmail.com>
2023-02-05 14:07:00 +0000

    Level 1 Mostly Completed
    Yoo

--- GameJam2D/Assets/Key.cs
+++ GameJam2D/Assets/Key.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    private PlayerInventory inventory;
+    [SerializeField]private PlayerInventory inventory;
 
     private void Start()
     {
